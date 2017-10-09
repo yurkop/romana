6d31bbd26ca7dcb44996200e37a3c9c25fbe90ad
yurkop <ykopatch@mail.ru>
2017-10-09 18:51:23 +0300

    updated classdef in toptions.h

--- src/toptions.h
+++ src/toptions.h
@@ -211,7 +211,7 @@ class Toptions: public TObject {
   //void GetPar(const char* name, Int_t module, Int_t i, Int_t &par, Int_t &min, Int_t &max);
 
 
-  ClassDef(Toptions, 98)
+  ClassDef(Toptions, 99)
 };
 
 ClassImp(Toptions)
